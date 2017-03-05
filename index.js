ac634a75f14a32d597685775626240529124329b
Derek P Sifford <dereksifford@gmail.com>
2017-03-05 14:51:51 -0500

    add underscore pattern

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function detectRepoTestFiles(dir, options) {
 
     return globby([
         '**/{test,spec}?(s)/**/*',            // Conventional test dirs (including deep)
-        '**/*[.-]{test,spec}?(s).*',          // Suffix-style test files (foo-test.js, foo.test.js)
+        '**/*[.-_]{test,spec}?(s).*',         // Suffix-style test files (foo-test.*, foo.test.*, foo_test.*)
         '**/{test,spec}?(s).*',               // Conventional root test files
         '**/_?(_){test,spec}?(s)?(_)_/**/*',  // React/Jest style test files
     ], options)
